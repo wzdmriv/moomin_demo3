ee5b0524f8257f975ad760d1b0625ffcafe517cc
wzdmriv <wzdmriv@users.noreply.github.com>
2020-11-25 00:25:19 +0900

    1

--- js/main.js
+++ js/main.js
@@ -32,3 +32,31 @@ function closeStart(){
     var fade_speed = 500;
     $(".start").fadeOut(fade_speed);
 }
+
+
+function centeringModalSyncer() {
+
+    //画面(ウィンドウ)の幅、高さを取得
+    var w = $( window ).width() ;
+    var h = $( window ).height() ;
+
+    // コンテンツ(#modal-content)の幅、高さを取得
+    // jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
+    //var cw = $( "#modal-content" ).outerWidth( {margin:true} );
+    //var ch = $( "#modal-content" ).outerHeight( {margin:true} );
+    var cw = $( "#modal-content" ).outerWidth();
+	var ch = $( "#modal-content" ).outerHeight();
+	var ccw = $( "#howtopop" ).outerWidth();
+	var cch = $( "#howtopop" ).outerHeight();
+	var cccw = $( "#passwordconf" ).outerWidth();
+    var ccch = $( "#passwordconf" ).outerHeight();
+
+    //センタリングを実行する
+    $( "#star_11, #star_21" ).css( {"left": (h*0.48) + "px","top": (h*0.8) + "px"} ) ;
+    $( "#star_12, #star_22" ).css( {"left": (h*0.48) + "px","top": (h*0.73) + "px"} ) ;
+    $( "#star_13, #star_23" ).css( {"left": (h*0.38) + "px","top": (h*0.58) + "px"} ) ;
+    $( "#star_14, #star_24" ).css( {"left": (h*0.62) + "px","top": (h*0.31) + "px"} ) ;
+    $(document).ready(centeringModalSyncer());
+}
+
+$( window ).resize( centeringModalSyncer ) ;
