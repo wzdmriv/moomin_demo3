e7647b5f7bcdfd318dc662cfdc9689f33e13934b
wzdmriv <wzdmriv@gmail.com>
2020-11-29 11:26:25 +0900

    1

--- js/main.js
+++ js/main.js
@@ -1,32 +1,3 @@
-(function(){
-    // 設定
-    var testTimer;
-    var interval =10000; // 切り替わりの間隔（ミリ秒）
-    var fade_speed = 500;// フェード処理の早さ（ミリ秒）
-
-    
-    var changeImage = function(){
-            var $active = $(".fade-img-box img.active");
-            var $next = $active.next("img").length?$active.next("img"):$(".fade-img-box img:first");
-            $active.fadeOut(fade_speed).removeClass("active");
-            $next.fadeIn(fade_speed).addClass("active");
-            if($next.attr("src")=="prt6.png"){
-                stopTimer();
-            }
-    }
-
-    function startTimer(){
-        $("#img").hide();
-        $(".fade-img-box img:first").addClass("active").show();
-        testTimer=setInterval(changeImage, interval);
-    }
-    function stopTimer(){
-        clearInterval(testTimer);
-    }
-    
-    $(document).ready(startTimer());
-}());
-
 function closeStart(){
     $(".map").fadeIn("fast");
     $(".start").fadeOut("fast");
@@ -48,9 +19,19 @@ function deletelm(lm_index){
     $(".landmark,#modal-overlay_lm,#lm" + lm_index).fadeOut("fast");
 }
 
-function gotoAR(){
+function gotoAR(ar_index){
     $(".map").fadeOut("fast");
     $( ".ar" ).css( {"visibility": "visible"} ) ;
+    $("#lmfade_sub" + ar_index).fadeIn("fast");
+    $("#img").hide();
+    $("#fade_sub" + ar_index + "img:first").addClass("active").show();
+    $('.ar').on('click', function() {
+        var $active = $(".fade-img-box img.active");
+        var $next = $active.next("img").length?$active.next("img"):$(".fade-img-box img:first");
+        $active.fadeOut("fast").removeClass("active");
+        $next.fadeIn("fast").addClass("active");
+        }
+    );
 }
 
 
